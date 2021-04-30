f6d5eec90e08d9aefed7806361fa4a7773df5502
Alena Volarevic <alenavolarevic@gmail.com>
2021-04-29 22:38:16 -0400

    Add ed the useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -23,7 +23,7 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <Card><button onClick={clickHandler}>Change Title</button></Card>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
