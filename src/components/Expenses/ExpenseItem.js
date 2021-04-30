67a3dc855ce6b50bd148e1b9c3715e5872c8dcb2
Alena Volarevic <alenavolarevic@gmail.com>
2021-04-29 22:37:43 -0400

    Add ed the useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,29 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  // useState - react hook, must be called inside of component function not nested or outside.
+  const [title, setTitle] = useState(props.title); // returns updated value and function to change
+  // title is pointer to props.title, setTitle function that changes the value.
+  // using useState forces component to be reevaluated! Works per component instance!
+
+  const clickHandler = () => {
+    setTitle("Updated");
+    console.log(title);
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <Card><button onClick={clickHandler}>Change Title</button></Card>
     </Card>
   );
 }
